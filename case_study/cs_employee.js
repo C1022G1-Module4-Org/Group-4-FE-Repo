e8a7fba9acb1929cd6be226f093a1956346b1233
levanphatdat <levanphatdat9@gmail.com>
2023-03-21 09:50:14 +0700

    em Dat up

--- case_study/cs_employee.js
+++ case_study/cs_employee.js
@@ -55,10 +55,12 @@ function renderEmployee(employee) {
         <td><button onclick=getEmployee('${employees.id}','${employees.name}') id="deleteId" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
         <i class="fas fa-trash-alt"></i>
         </button>
-        <button class="btn btn-primary btn-sm edit" type="button" title="Sửa"
-        id="show-emp" data-toggle="modal" data-target="#update">
-        <i class="fas fa-edit"></i>
-        </button></td>
+         <button class="btn btn-primary btn-sm edit" type="button" title="Sửa" 
+                id="show-emp" data-toggle="modal" data-target="#update"
+                onclick="getInfo(${employees.id})">
+                <i class="fas fa-edit"></i>
+            </button>
+        </td>
        </tr>`;
     }
     $("#listEmployee").html(element);
@@ -129,7 +131,6 @@ function addEmployee(name, gender, positionDTO, email, dateOfBirth, phoneNumber,
             email: email,
             address: address,
             phoneNumber: phoneNumber
-
         }),
         success: function (data) {
             alert("Thêm nhân viên thành công!");
@@ -156,7 +157,7 @@ function add() {
     addEmployee(name, gender, positionDTO, email, dateOfBirth, phoneNumber, address);
 };
 
-function getSelectPosition() {
+function getSelectPosition(id) {
     $.ajax({
         type: "GET",
         url: `http://localhost:8080/position`,
@@ -165,6 +166,22 @@ function getSelectPosition() {
         },
         success: function (data) {
             showPositionSelectOption(data);
+        },
+        error: function (error) {
+            console.log(error);
+        },
+    });
+}
+
+function getSelectPositionForUpdate(id) {
+    $.ajax({
+        type: "GET",
+        url: `http://localhost:8080/position`,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        success: function (data) {
+            showPositionSelectOptionUpdate(data, id);
         },
         error: function (error) {
             console.log(error);
@@ -191,4 +208,160 @@ $(document).ready(function () {
     getSelectPosition();
 });
 
+function editEmployee(id, name, gender, positionDTO, email, dateOfBirth, phoneNumber, address) {
+    debugger
+    $.ajax({
+        type: "PUT",
+        url: `http://localhost:8080/employee/edit/` + id,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        data: JSON.stringify({
+            id: id,
+            name: name,
+            positionDTO: {
+                id: positionDTO
+            },
+            dateOfBirth: dateOfBirth,
+            gender: gender,
+            email: email,
+            address: address,
+            phoneNumber: phoneNumber
+
+        }),
+        success: function () {
+            alert("Sửa thông tin nhân viên thành công!");
+            $("#edit1").hide();
+            $('body').removeClass("modal-open");
+            $(".modal-backdrop").remove();
+            loadList();
+        },
+        error: function () {
+            alert("Lỗi khi sửa thông tin nhân viên !!!")
+        }
+    })
+}
+
+$("#update-performing").submit(function () {
+    debugger
+    let id = $("#update-id").val();
+    let name = $("#edit-name").val();
+    let gender = $('input[name="edit-gender"]:checked').val();
+    let email = $("#edit-email").val();
+    let positionDTO = $("#position1").val();
+    // name, gender, positionDTO, email, dateOfBirth, phoneNumber, address
+    let dateOfBirth = $("#edit-dateOfBirth").val();
+    let phoneNumber = $("#edit-phoneNumber").val();
+    let address = $("#edit-address").val();
+    editEmployee(id,name, gender, positionDTO, email, dateOfBirth, phoneNumber, address);
+})
+
+function getInfo(id) {
+    $.ajax({
+        type: "GET",
+        url: `http://localhost:8080/employee/info/` + id,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        success: function (data) {
+            getSelectPositionForUpdate(id);
+            let element = "";
+            let employee = data;
+            element +=
+                `
+            <div class="modal-body">
+                <div class="form-group">
+                    <div id="thongbao1" class="text-danger" style="text-align: center;"></div>
+                </div>
+                <div class="form-group">
+                <input type="hidden" id="update-id" value="${employee.id}">
+                </div>
+                <div class="form-group">
+                    <label for="name" class="control-label col-xs-3">Tên nhân viên<span
+                            class="span">*</span></label>
+                    <div class="col-md-12">
+                        <input required type="text" class="form-control" id="edit-name" name="edit-name" value="${employee.name}">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Chức vụ<span class="span">*</span></label>
+                    <div class="col-md-12" id="positionDTO1">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="dateOfBirth" class="control-label col-xs-3">Ngày sinh<span class="span">*</span></label>
+                    <div class="col-md-12">
+                        <input required type="date" class="form-control" id="edit-dateOfBirth" name="edit-dateOfBirth" value="${employee.dateOfBirth}">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-xs-3">Giới tính<span
+                            class="span">*</span></label>
+                    <div class="col-md-12">
+                        <input required type="radio" class="" id="edit-men" name="edit-gender" value="true">
+                        <label for="edit-men">Nam</label>
+                        <input required type="radio" class="" id="edit-women" name="edit-gender" value="false">
+                        <label for="edit-women">Nữ</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="edit-email" class="control-label col-xs-3">Email<span class="span">*</span></label>
+                    <div class="col-md-12">
+                        <input required type="text" class="form-control" id="edit-email" name="edit-email" value="${employee.email}">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="edit-phoneNumber" class="control-label col-xs-3">Số điện thoại<span
+                            class="span">*</span></label>
+                    <div class="col-md-12">
+                        <input required type="text" class="form-control" id="edit-phoneNumber" name="edit-phoneNumber" value="${employee.phoneNumber}">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="edit-address" class="control-label col-xs-3">Địa chỉ<span class="span">*</span></label>
+                    <div class="col-md-12">
+                        <input required type="text" class="form-control" id="edit-address" name="edit-address" value="${employee.address}">
+                    </div>
+                </div>
+                <div style="border: 0" class="modal-footer col-md-5 text-center">
+                    <button type="submit" id="btnEdit" class="btn btn-success">Lưu</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Hủy bỏ</button>
+                </div>
+            </div>`
+            $("#update-performing").html(element);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    })
+}
+function showPositionSelectOptionUpdate(positions, id) {
+    let element = "";
+    let selectPosition="";
+    for (let position of positions) {
+        for (let employee of position.employeeSet) {1
+            if (employee.id === id) {
+                selectPosition = position.position;
+            }
+        }
+    }
+    element += `
+  <select class="form-control" id="position1" name="position1">
+ <option value="${id}">${selectPosition}</option>
+`
+    ;
+
+    for (let position of positions) {
+        if (position.position!== selectPosition){
+            element += `<option value="${position.id}">`;
+            element += position.position;
+            `</option>`;
+        }
+    }
+
+    `</select>`;
+    $("#positionDTO1").html(element);
+}
 
