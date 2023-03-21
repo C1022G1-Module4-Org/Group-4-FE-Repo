ac111b58c794a421179c326e207d97500b4bf1d7
dinhminhthong <dinhminhthong14022001@gmail.com>
2023-03-21 08:13:51 +0700

    up

--- case_study/js.shop.js
+++ case_study/js.shop.js
@@ -1,21 +1,65 @@
+//phân trangggg
+function movePage (page) {
+    showList(page);
+}
 
+function renderPage (branchList) {
+
+    let pageable = "";
+    if (branchList.number <= branchList.totalPages -1 && branchList.number > 0) {
+        pageable += `
+    <button class="page-item btn btn-dark" 
+    onclick="movePage(${branchList.number - 1})">
+    <i class="ti-angle-left"></i>
+    </button>
+    `
+    }
+    for (let i =1; i <= branchList.totalPages; i++) {
+        let pageItem = $(`<button class="page-item number btn btn-dark"
+                      onclick="movePage(${i - 1})">
+                      ${i}
+                      </button>`);
+        if (i === branchList.number + 1) {
+            pageItem.addClass("active");
+        } else {
+            pageItem.removeClass("active");
+        }
+        pageable += pageItem.prop('outerHTML');
+    }
+
+    if (branchList.number < 0 && branchList.number < branchList.totalPages) {
+        pageable += `
+    <button class="page-item btn btn-dark" 
+    onclick="movePage(${branchList.number + 1})">
+    <i class="ti-angle-right"></i>
+    </button>
+    `
+    }
+    $("#pagination").html(pageable);
+}
+
+//showList
 function getBranchIdAndName(id, name) {
     document.getElementById("deleteId").value = id;
     document.getElementById("deleteName").innerText =
         "Bạn có muốn xóa chi nhánh " + name + "?";
 }
-function showList() {
+function showList(page) {
+
+    let search = $("#search").val();
     // debugger
     $.ajax({
         type:"GET",
         dataType:"json",
-        url:`http://localhost:8080/branch`,
+        url:`http://localhost:8080/branch?name=${search}&page=${page ? page : 0}`,
         success: function (data) {
+            renderPage(data)
             data=data.content
             let contents = "";
+            let stt = 1;
             for (let i = 0; i < data.length; i++) {
                 contents += `<tr>`
-                contents += `<td>${data[i].id}`
+                contents += `<td>${stt++}`
                 contents += `<td>${data[i].name}`
                 contents += `<td>${data[i].email}`
                 contents += `<td>${data[i].phone}`
@@ -29,7 +73,8 @@ function showList() {
                 <i class="fas fa-trash-alt"></i>
               </button>
                                     <button class="btn btn-primary btn-sm edit" type="button" title="Sửa"
-                                            id="show-emp" data-toggle="modal" data-target="#update">
+                                            id="show-emp" data-toggle="modal" data-target="#update"
+                                            onclick="getBranchInfo(${data[i].id})">
                                         <i class="fas fa-edit"></i>
                                     </button>
                                
@@ -88,6 +133,7 @@ function add () {
 
 
 function addBranch(name, email, phone, address) {
+    debugger
     $.ajax({
         headers: {
             Accept: "application/json",
@@ -149,4 +195,103 @@ function showCoffeShopSelectOption(coffeShop) {
 $(document).ready(function () {
     getSelectCoffeShopList();
 });
+//updateeeeee
+function update(){
+
+    let id = $("#update-id").val();
+    let name = $("#update-name").val();
+    let email = $("#update-email").val();
+    let phone = $("#update-phone").val();
+    let address = $("#update-address").val();
+
+    updateBranch(id, name, email, phone, address);
+};
+
+function updateBranch(id, name, email, phone, address) {
+
+    $.ajax ({
+        type: "PUT",
+        url: `http://localhost:8080/branch/edit/${id}`,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        data: JSON.stringify({
+            id: id,
+            name: name,
+            email: email,
+            phone: phone,
+            address: address,
+            coffeeShopDTO: {id:1}
+        }),
+        success: function (data) {
+            alert("Sửa chi nhánh thành công!");
+            $("#update").hide();
+            $("body").removeClass("modal-open");
+            $(".modal-backdrop").remove();
+            showList();
+        },
+        error: function () {
+            alert("Lỗi khi sửa chi nhánh!");
+        },
+    })
+}
+
+function getBranchInfo(id) {
+    $.ajax({
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        type: "get",
+        url: `http://localhost:8080/branch/detail/${id}`,
+        success: function (data) {
+            let element = "";
+            let branch = data;
+            element +=
+                `
+      <div class="form-group">
+        <div id="thongbao" class="text-danger" style="text-align: center;"></div>
+      </div>
+      <div class="form-group">
+        <input type="hidden" id="update-id" value="${branch.id}">
+      </div>
+      <div class="form-group">
+        <label for="update-name" class="control-label col-xs-3">Tên chi nhánh</label>
+        <div class="col-md-12">
+          <input type="text" class="form-control" id="update-name" value="${branch.name}">
+        </div>
+      </div>
+      <div class="form-group">
+       <label for="update-email" class="control-label col-xs-3">Email</label>
+        <div class="col-md-12">
+          <input type="text" class="form-control" id="update-email" value="${branch.email}">
+        </div>
+      </div>
+      <div class="form-group">
+      <label for="update-phone" class="control-label col-xs-3">Số điện thoại</label>
+        <div class="col-md-12">
+          <input type="text" class="form-control" id="update-phone" value="${branch.phone}">
+      </div>
+      <div class="form-group">
+        <label for="update-address" class="control-label col-xs-3">Địa chỉ mới</label>
+        <div class="col-md-12">
+          <input type="text" class="form-control" id="update-address" value="${branch.address}">
+        </div>
+      </div>
+                          <div style="border: 0" class="modal-footer col-md-5 text-center">
+                        <button type="button" id="btnSave" class="btn btn-success" onclick="update()">Lưu</button>
+                        <button class="btn btn-danger" data-dismiss="modal">Hủy bỏ</button>
+                    </div>
+      `
+            $("#update-form").html(element);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    })
+}
+
+
+
 
